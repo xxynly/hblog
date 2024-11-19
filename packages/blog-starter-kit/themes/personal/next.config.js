094d1f9c873406e87a9d587f67ff2b4afafe4ec7
xxynly <71076952+xxynly@users.noreply.github.com>
2024-11-19 10:20:19 +0800

    Update next.config.js

--- packages/blog-starter-kit/themes/personal/next.config.js
+++ packages/blog-starter-kit/themes/personal/next.config.js
@@ -81,6 +81,14 @@ const config = {
 				source: '/api/analytics',
 				destination: `${HASHNODE_ADVANCED_ANALYTICS_URL}/api/analytics`,
 			},
+      {
+        source: "/blog",
+        destination: "https://starter-kit-rose-seven.vercel.app/blog", -> Replace https://starter-kit-rose-seven.vercel.app with your own Vercel deployment URL from step 1
+      },
+      {
+        source: "/blog/:path*",
+        destination: "https://starter-kit-rose-seven.vercel.app/blog/:path*", -> Replace https://starter-kit-rose-seven.vercel.app with your own Vercel deployment URL from step 1
+      },
 		];
 	},
 	async redirects() {
