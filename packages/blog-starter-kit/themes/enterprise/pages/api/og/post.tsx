fc5dc0953d42ab0d21f9633b802e434bb1e6ce4b
xxynly <xxynly@gmail.com>
2025-03-23 13:59:35 +0800

    style(og): 禁用 Next.js 图片元素的 ESLint 规则
    为了在 OG 图片生成 API 中使用 `<img>` 元素，禁用了 `@next/next/no-img-element` 的 ESLint 规则。这不会影响功能，但允许在生成动态图片时使用更灵活的 HTML 元素。

--- packages/blog-starter-kit/themes/enterprise/pages/api/og/post.tsx
+++ packages/blog-starter-kit/themes/enterprise/pages/api/og/post.tsx
@@ -1,4 +1,4 @@
-import { resizeImage } from '@starter-kit/utils/image';
+/* eslint-disable @next/next/no-img-element */
 import { ImageResponse } from '@vercel/og';
 import { type NextRequest } from 'next/server';
 import { DEFAULT_AVATAR } from '../../../utils/const';
